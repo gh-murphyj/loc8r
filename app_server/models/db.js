71bbf7191a3caa35bf6cb0ba6c2a6cea4f7470d1
Mr Moo <jemn@hotmail.com>
2016-04-06 00:35:02 +0100

    Added code to gracefully disconnect from the database when node is terminated or restarted

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,48 @@
 var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost/loc8r';
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose connected to ' + dbURI);
+})
+
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connection error ' + err);
+})
+
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose discconnected from ' + dbURI);
+})
+
+var gracefulShutdown = function(msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose discconnected through ' + msg);
+    callback();
+  });
+ };
+
+process.once('SIGUSR2', function() {
+  gracefulShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+/* To use SIGINT on some versions of Windows it would be
+necessary to add the npm package readline and to add some
+code to check for the app termination and emit a SIGINT for 
+the following to work. I'm only deploying on Linux though so
+dont need it. See page 126 for more info */
+
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function () {
+    process.exit(0);
+  });
+});
+
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku app shutdown', function () {
+    process.exit(0);
+  });
+});
+ 
